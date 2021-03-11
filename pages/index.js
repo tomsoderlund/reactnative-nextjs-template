22b375884cc3a29700ea027232e824776d4b1bbc
Tom Söderlund <tom@tomsoderlund.com>
2021-03-11 10:58:53 +0100

    Default text

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      <Text style={styles.text}>reactnative-nextjs-template</Text>
     </View>
   )
 }
