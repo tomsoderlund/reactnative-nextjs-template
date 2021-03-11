1d09c73ad3e9bf542b0f99cce0452eaac876a9fa
Tom Söderlund <tom@tomsoderlund.com>
2021-03-11 22:12:01 +0100

    Fix crash issue

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import React from 'react'
 // @generated: @expo/next-adapter@2.1.5
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>reactnative-nextjs-template</Text>
+      <Text style={styles.text}>This is reactnative-nextjs-template running!</Text>
     </View>
   )
 }
