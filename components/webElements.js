d28837b369d3f8cf91d7f69a69a903778214902c
Tom Söderlund <tom@tomsoderlund.com>
2021-03-12 10:00:43 +0100

    H1-3 for SEO

---
+++ components/webElements.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export const H1 = (props) => <Heading {...props} level={1} />
+export const H2 = (props) => <Heading {...props} level={2} />
+export const H3 = (props) => <Heading {...props} level={3} />
+
+export const Heading = (props) => {
+  const { children, level, ...otherProps } = props
+  return (
+    <Text
+      {...otherProps}
+      accessibilityRole='heading'
+      aria-level={level}
+    >
+      {children}
+    </Text>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 // @generated: @expo/next-adapter@2.1.5
 import { StyleSheet, Text, View } from 'react-native'
 
+import { H1 } from '../components/webElements'
+
 export default function App () {
   return (
     <View style={styles.container}>
+      <H1 style={styles.h1}>Heading H1</H1>
       <Text style={styles.text}>This is reactnative-nextjs-template running!</Text>
     </View>
   )
@@ -18,5 +21,11 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 16
+  },
+  h1: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 12,
+    marginBottom: 12
   }
 })
