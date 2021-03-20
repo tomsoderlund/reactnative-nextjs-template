527da7a4e71a8d6bb1f33f145eeaabd5d765b3d9
Tom Söderlund <tom@tomsoderlund.com>
2021-03-20 11:21:07 +0100

    webpack config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@
 
 const { withExpo } = require('@expo/next-adapter')
 
-module.exports = withExpo({
-  projectRoot: __dirname
-})
+const nextConfig = {
+  projectRoot: __dirname,
+
+  // For font support:
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+      loader: 'file-loader?name=assets/[name].[hash].[ext]'
+    })
+    return config
+  }
+}
+
+module.exports = withExpo(nextConfig)
