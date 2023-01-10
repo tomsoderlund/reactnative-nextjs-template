d822d3a3a820bb0f762378a1a45b0649cc815714
Tom Söderlund <tom@tomsoderlund.com>
2023-01-10 22:08:00 +0100

    Minimal working app (no video or React Native Elements)

--- components/page/Page.js
+++ components/page/Page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Header } from 'react-native-elements'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+// import { Header } from 'react-native-elements'
+// import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { config } from '../../config/config'
 
@@ -9,20 +9,22 @@ import PageHead from './PageHead'
 
 const Page = ({ title = config.appName, description, children }) => {
   return (
-    <SafeAreaProvider>
+    <>
       <PageHead
         title={title}
         description={description}
       />
+      {/*
       <Header
         leftComponent={{ icon: 'menu', color: '#fff' }}
         centerComponent={{ text: title, style: { color: '#fff' } }}
         rightComponent={{ icon: 'home', color: '#fff' }}
       />
+       */}
       <View style={styles.container}>
         {children}
       </View>
-    </SafeAreaProvider>
+    </>
   )
 }
 export default Page
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text } from 'react-native'
 
 import Page from '../components/page/Page'
 import { H1 } from '../components/webElements'
-import VideoPlayer from '../components/VideoPlayer'
+// import VideoPlayer from '../components/VideoPlayer'
 
 import { config } from '../config/config'
 
@@ -13,9 +13,6 @@ export default function StartPage () {
       <H1 style={styles.h1}>Heading H1</H1>
       <Text style={styles.text}>This is {config.appName} running</Text>
       <Text style={styles.text}>{config.appTagline}</Text>
-      <VideoPlayer
-        videoUrl='https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'
-      />
     </Page>
   )
 }
