19a9d95fa805c9318110d40f64522bb32beb90a8
Tom Söderlund <tom@tomsoderlund.com>
2023-04-04 15:32:40 +0200

    app.json

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App (): React.ReactElement {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>React Native + Next.js</Text>
       <StatusBar style='auto' />
     </View>
   )
